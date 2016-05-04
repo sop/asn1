6dfabcf6e22312e536950e9789d8ca935c7f8199
Joni Eskelinen <joni@mobie.fi>
2016-05-04 09:41:12 +0300

    Add comments

--- lib/ASN1.php
+++ lib/ASN1.php
@@ -1,21 +1,26 @@
 <?php
 
 
-class ASN1
+abstract class ASN1
 {
 	/**
-	 * Register standalone autoloader
+	 * Register standalone autoloader.
+	 *
+	 * This library is compatible with PSR-4 autoloaders and works seamlessly
+	 * with composer. Standalone autoloader here is provided in case
+	 * one does not use composer or other means to autoload classes from
+	 * this namespace.
 	 */
 	public static function registerAutoloader() {
 		spl_autoload_register(function ($cls) {
 			static $prefix = __CLASS__;
-			// check that requested class is in the same namespace
+			// check that the requested class is in the same namespace
 			if (substr($cls, 0, strlen($prefix)) !== $prefix) {
 				return false;
 			}
 			$path = __DIR__ . DIRECTORY_SEPARATOR .
 				 str_replace("\\", DIRECTORY_SEPARATOR, $cls) . ".php";
-			// check that file exists
+			// check that the file exists
 			if (!is_file($path)) {
 				return false;
 			}
@@ -23,7 +28,7 @@ class ASN1
 			if (!(include_once $path)) {
 				return false;
 			}
-			// check that requested class exists after include
+			// check that the requested class now exists
 			if (!class_exists($cls, false)) {
 				return false;
 			}
