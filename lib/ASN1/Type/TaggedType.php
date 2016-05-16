90efa85d53ffe9586e4d52108e7c5818690a45e9
Joni Eskelinen <joni@mobie.fi>
2016-05-16 09:46:42 +0300

    Add tagging expectation methods

--- lib/ASN1/Type/TaggedType.php
+++ lib/ASN1/Type/TaggedType.php
@@ -6,6 +6,8 @@ use ASN1\Component\Identifier;
 use ASN1\Component\Length;
 use ASN1\Element;
 use ASN1\Type\Tagged\DERTaggedType;
+use ASN1\Type\Tagged\ExplicitTagging;
+use ASN1\Type\Tagged\ImplicitTagging;
 
 
 /**
@@ -20,5 +22,41 @@ abstract class TaggedType extends Element
 		$length = Length::expectFromDER($data, $idx);
 		$offset = $idx + $length->length();
 		return $type;
+	}
+	
+	/**
+	 * Check whether element supports explicit tagging.
+	 *
+	 * @param int|null $expectedTag Optionally checked tag
+	 * @throws \UnexpectedValueException
+	 * @return self
+	 */
+	public function expectExplicit($expectedTag = null) {
+		if (!($this instanceof ExplicitTagging)) {
+			throw new \UnexpectedValueException(
+				"Element doesn't implement explicit tagging.");
+		}
+		if (isset($expectedTag)) {
+			$this->expectTagged($expectedTag);
+		}
+		return $this;
+	}
+	
+	/**
+	 * Check whether element supports implicit tagging.
+	 *
+	 * @param int|null $expectedTag
+	 * @throws \UnexpectedValueException
+	 * @return self
+	 */
+	public function expectImplicit($expectedTag = null) {
+		if (!($this instanceof ImplicitTagging)) {
+			throw new \UnexpectedValueException(
+				"Element doesn't implement implicit tagging.");
+		}
+		if (isset($expectedTag)) {
+			$this->expectTagged($expectedTag);
+		}
+		return $this;
 	}
 }
