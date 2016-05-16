ba899ffcfd02bbf3899d26453245b363ddc4fdd9
Joni Eskelinen <joni@mobie.fi>
2016-05-16 14:33:28 +0300

    Add fromString to TimeType

--- lib/ASN1/Type/TimeType.php
+++ lib/ASN1/Type/TimeType.php
@@ -27,11 +27,58 @@ abstract class TimeType extends Element
 	}
 	
 	/**
+	 * Initialize from datetime string.
+	 *
+	 * @param string $time Time string
+	 * @param string|null $tz Timezone, if null use default.
+	 * @throws \RuntimeException
+	 * @return self
+	 */
+	public static function fromString($time, $tz = null) {
+		try {
+			if (!isset($tz)) {
+				$tz = date_default_timezone_get();
+			}
+			return new static(
+				new \DateTimeImmutable($time, self::_createTimeZone($tz)));
+		} catch (\Exception $e) {
+			throw new \RuntimeException(
+				"Failed to create DateTime: " .
+					 self::_getLastDateTimeImmutableErrorsStr(), 0, $e);
+		}
+	}
+	
+	/**
 	 * Get time.
 	 *
 	 * @return \DateTimeImmutable
 	 */
 	public function dateTime() {
 		return $this->_dateTime;
+	}
+	
+	/**
+	 * Create DateTimeZone object from string.
+	 *
+	 * @param string $tz
+	 * @throws \UnexpectedValueException
+	 * @return \DateTimeZone
+	 */
+	private static function _createTimeZone($tz) {
+		try {
+			return new \DateTimeZone($tz);
+		} catch (\Exception $e) {
+			throw new \UnexpectedValueException("Invalid timezone.", 0, $e);
+		}
+	}
+	
+	/**
+	 * Get last error caused by DateTimeImmutable.
+	 *
+	 * @return string
+	 */
+	private static function _getLastDateTimeImmutableErrorsStr() {
+		$errors = \DateTimeImmutable::getLastErrors()["errors"];
+		return implode(", ", $errors);
 	}
 }
