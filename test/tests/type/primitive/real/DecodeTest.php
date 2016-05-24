9920848e9634609d20dd27963fa05c6c9c4be642
Joni Eskelinen <joni@mobie.fi>
2016-05-24 13:24:38 +0300

    Add tests

--- test/tests/type/primitive/real/DecodeTest.php
+++ test/tests/type/primitive/real/DecodeTest.php
@@ -4,13 +4,64 @@ use ASN1\Type\Primitive\Real;
 
 
 /**
- * @group decode
+ * @group type
  * @group real
  */
 class RealDecodeTest extends PHPUnit_Framework_TestCase
 {
-	public function testType() {
-		$el = Real::fromDER("\x09\x0");
-		$this->assertInstanceOf(Real::class, $el);
+	/**
+	 * @expectedException RuntimeException
+	 */
+	public function testBinaryEncodingFail() {
+		$data = "\x9\x2\x80\x0";
+		Real::fromDER($data);
+	}
+	
+	/**
+	 * @expectedException RuntimeException
+	 */
+	public function testNonNR3DecimalEncodingFail() {
+		$data = "\x9\x02\x010";
+		Real::fromDER($data);
+	}
+	
+	/**
+	 * @expectedException ASN1\Exception\DecodeException
+	 */
+	public function testSpecialEncodingMultipleOctetsFail() {
+		$data = "\x9\x02\x40\x0";
+		Real::fromDER($data);
+	}
+	
+	/**
+	 * @expectedException RuntimeException
+	 */
+	public function testSpecialEncodingPositiveINF() {
+		$data = "\x9\x01\x40";
+		Real::fromDER($data);
+	}
+	
+	/**
+	 * @expectedException RuntimeException
+	 */
+	public function testSpecialEncodingNegativeINF() {
+		$data = "\x9\x01\x41";
+		Real::fromDER($data);
+	}
+	
+	/**
+	 * @expectedException ASN1\Exception\DecodeException
+	 */
+	public function testInvalidSpecialEncodingFail() {
+		$data = "\x9\x01\x4f";
+		Real::fromDER($data);
+	}
+	
+	/**
+	 * @expectedException ASN1\Exception\DecodeException
+	 */
+	public function testInvalidNumberFail() {
+		$data = "\x9\x02\x03.";
+		Real::fromDER($data);
 	}
 }
---
+++ test/tests/type/primitive/real/RealTest.php
@@ -0,0 +1,110 @@
+<?php
+
+use ASN1\Element;
+use ASN1\Type\Primitive\Real;
+
+
+/**
+ * @group type
+ * @group real
+ */
+class RealTest extends PHPUnit_Framework_TestCase
+{
+	public function testCreate() {
+		$el = new Real("314.E-2");
+		$this->assertInstanceOf(Real::class, $el);
+		return $el;
+	}
+	
+	/**
+	 * @depends testCreate
+	 *
+	 * @param Element $el
+	 */
+	public function testTag(Element $el) {
+		$this->assertEquals(Element::TYPE_REAL, $el->tag());
+	}
+	
+	/**
+	 * @depends testCreate
+	 *
+	 * @param Element $el
+	 */
+	public function testEncode(Element $el) {
+		$der = $el->toDER();
+		$this->assertInternalType("string", $der);
+		return $der;
+	}
+	
+	/**
+	 * @depends testEncode
+	 *
+	 * @param string $data
+	 */
+	public function testDecode($data) {
+		$el = Real::fromDER($data);
+		$this->assertInstanceOf(Real::class, $el);
+		return $el;
+	}
+	
+	/**
+	 * @depends testCreate
+	 * @depends testDecode
+	 *
+	 * @param Element $ref
+	 * @param Element $el
+	 */
+	public function testRecoded(Element $ref, Element $el) {
+		$this->assertEquals($ref, $el);
+	}
+	
+	/**
+	 * @dataProvider provideFromFloat
+	 *
+	 * @param float $number
+	 */
+	public function testFromFloat($number) {
+		$real = Real::fromFloat($number);
+		$recoded = Real::fromDER($real->toDER());
+		$this->assertEquals($number, $recoded->float());
+	}
+	
+	public function provideFromFloat() {
+		return array(
+			/* @formatter:off */
+			[0],
+			[1],
+			[-1],
+			[1000000],
+			[-1000000],
+			[0.00001],
+			[-0.00001],
+			[0.1e-20],
+			[-0.1e-20],
+			[M_PI],
+			[-M_PI],
+			[M_E],
+			[-M_E]
+			/* @formatter:on */
+		);
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testInvalidFormatFail() {
+		new Real("fail");
+	}
+	
+	/**
+	 * @expectedException UnexpectedValueException
+	 */
+	public function testNR3ToDecimalInvalidFormatFail() {
+		$real = new Real(Real::NR3_ZERO);
+		$cls = new ReflectionClass($real);
+		$prop = $cls->getProperty("_number");
+		$prop->setAccessible(true);
+		$prop->setValue($real, "fail");
+		$real->float();
+	}
+}
